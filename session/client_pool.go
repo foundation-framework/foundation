9c7afe420807e21bd9a81b7d1c381dd518abb24d
undefined7887 <undefined7887@gmail.com>
2021-09-23 19:59:38 +0300

    moved all *utils packages into utils folder

--- session/client_pool.go
+++ session/client_pool.go
@@ -3,7 +3,7 @@ package session
 import (
 	"github.com/Workiva/go-datastructures/set"
 	"github.com/intale-llc/foundation/net/sockets"
-	"github.com/intale-llc/foundation/utils/randutil"
+	"github.com/intale-llc/foundation/utils/rand-util"
 )
 
 type ClientPool struct {
