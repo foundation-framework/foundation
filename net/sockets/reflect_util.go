b23ed5753154e07ae45c0d01b7a4137614edbb99
undefined7887 <undefined7887@gmail.com>
2021-11-05 16:44:07 +0300

    net/sockets: removed duplicated function

--- net/sockets/reflect_util.go
+++ net/sockets/reflect_util.go
@@ -2,10 +2,6 @@ package sockets
 
 import "reflect"
 
-func isPointer(i interface{}) bool {
-	return reflect.TypeOf(i).Kind() == reflect.Ptr
-}
-
 func copyInterfaceValue(i interface{}) interface{} {
 	if reflect.TypeOf(i).Kind() == reflect.Ptr {
 		return reflect.New(reflect.ValueOf(i).Elem().Type()).Interface()
